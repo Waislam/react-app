057e65c97c017ecbffad0e36c461dc7c89c9f8ff
Waislam <waislam67@gmail.com>
2023-02-23 13:54:56 +0600

    text added

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,11 +1,14 @@
 // import React from "react";
 
 
-const HomePage=()=>{
+const HomePage = () => {
 
-    return(
+    return (
         <>
-            <div className="bg-green" style={{paddingTop:"10%"}}><h1>this is home page</h1></div>
+            <div className="bg-green" style={{ paddingTop: "10%" }}>
+                <h1>this is home page</h1>
+                <h3>No more wait.. now practice parctice and practice</h3>
+            </div>
         </>
     )
 }
